1b2519e0f71e9e4c333369e5bfe914028d7c9f02
Dim <cqd2202@yahoo.de>
2020-01-07 21:36:39 +0100

    PROMAN-00002: Project data structures

---
+++ de.dc.projectfx/src/main/java/de/dc/projectfx/model/ProjectType.java
@@ -0,0 +1,40 @@
+package de.dc.projectfx.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ProjectType {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private LocalDateTime createdOn;
+	
+	public ProjectType() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDateTime getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(LocalDateTime createdOn) {
+		this.createdOn = createdOn;
+	}
+}
