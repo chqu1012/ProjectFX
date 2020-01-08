8d85713e85f53f5d48ea28c6ce70d0eac3c5b719
Dim <cqd2202@yahoo.de>
2020-01-08 21:59:25 +0100

    PROMAN-00007: Added CalendarFX example to controller

---
+++ de.dc.projectfx/src/main/java/de/dc/projectfx/controller/BaseMainController.java
@@ -0,0 +1,10 @@
+package de.dc.projectfx.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+
+public abstract class BaseMainController {
+
+    @FXML
+    protected SplitPane paneAgenda;
+
+}
